6c74bf3ce93efe4ce493e736e7816c56ec3aab23
Brendan Toolan <btoolan73@gmail.com>
2019-10-30 19:12:09 +0000

    First DRaft

--- approx-sort.py
+++ approx-sort.py
@@ -5,15 +5,14 @@ def sqrt(x):
     z = 1.0
     # keep getting better estimamte for sq root 
     # of x until within two decimal places.
-
-    while abs(z*z -x) >= 0.01:
+    while abs(z*z - x) >= 0.0001:
         #get better approxication for sq root
         z -= (z*z -x) / (2*z)
     #return z
     return z
 
 #calculate sq root of 8
-z = sqrt(64.0)
+z = sqrt(100001.0)
 #print z 
 print(z)
 #print(sqrt(8.0))
